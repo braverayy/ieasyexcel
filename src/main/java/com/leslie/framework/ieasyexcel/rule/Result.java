528cad63b6587dcc6f811a56b5b713387bd032be
leslie <leslie.hudy@gmail.com>
2021-05-16 22:14:44 +0800

    add rule support

---
+++ src/main/java/com/leslie/framework/ieasyexcel/rule/Result.java
@@ -0,0 +1,17 @@
+package com.leslie.framework.ieasyexcel.rule;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author leslie
+ * @date 2021/5/15
+ */
+@Getter
+@Setter
+public class Result {
+
+    private boolean available;
+
+    private String msg;
+}
